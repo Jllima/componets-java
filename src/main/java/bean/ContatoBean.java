8d9e6c0c9f6b0cd7a6bebf3259ba750b57e74878
Jorge Luis <jlimajorge@gmail.com>
2016-07-27 15:09:45 -0300

    Adicionando framework jsf e executando primeiro exemplo

---
+++ src/main/java/bean/ContatoBean.java
@@ -0,0 +1,12 @@
+package bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "contato")
+public class ContatoBean {
+
+	public ContatoBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/bean/ExampleBean.java
@@ -0,0 +1,26 @@
+package bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ExampleBean {
+
+	public ExampleBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private String text;
+
+	public String upCaseText() {
+		this.text = this.text.toUpperCase();
+		return "show?faces-redirect=true";
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+}
